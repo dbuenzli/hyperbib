8fdef024919e2f786339edcf4ed5034560787e5c
Daniel Bünzli <daniel.buenzli@erratique.ch>
2022-09-19 16:03:13 +0200

    Change backup naming scheme.

--- src/db.ml
+++ src/db.ml
@@ -48,7 +48,7 @@ let pool ?read_only file ~size =
 let stamped_backup_file file =
   let stamp =
     let (t : Unix.tm) = Unix.localtime (Unix.gettimeofday ()) in
-    Fmt.str "-%04d-%02d-%02d--%02d-%02d-%02d"
+    Fmt.str "-%04d-%02d-%02d-%02d%02d%02d"
       (t.tm_year + 1900) (t.tm_mon + 1) t.tm_mday t.tm_hour t.tm_min t.tm_sec
   in
   let p, ext = Fpath.cut_ext ~multi:true file in
