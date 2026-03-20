f75f393ae7f873133b5e3fbd0ddba1f0056c38b3
Daniel Bünzli <daniel.buenzli@erratique.ch>
2026-03-20 20:51:31 +0100

    Static file service cosmetics

--- src/service/static_file_service.ml
+++ src/service/static_file_service.ml
@@ -10,26 +10,24 @@ let v env _sess request =
   let static_dir = Fpath.to_string (Service_env.static_dir env) in
   let* file = Http.Request.to_absolute_filepath ~file_root:static_dir request in
   let file = Fpath.v file in
-  let file = match Fpath.is_syntactic_dir file with
-  | true -> (* note because of path cleaning this is only for / *) file
-  | false ->
-      match Fpath.take_ext ~multi:false file with
-      | "" -> Fpath.(file + ".html")
-      | _ -> file
+  let file =
+    if Fpath.is_syntactic_dir file
+    then (* note because of path cleaning this is only for / *) file else
+    if Fpath.exists_ext file then file else Fpath.(file + ".html")
   in
   let dir_response = Webs_fs.dir_index_file "index.html" |> Result.get_ok in
-  let* resp = Webs_fs.send_file ~dir_response request (Fpath.to_string file) in
-  (* FIXME do something nice in send_file maybe *)
-  let resp = match Fpath.take_ext ~multi:false file with
+  let* response =
+    Webs_fs.send_file ~dir_response request (Fpath.to_string file)
+  in
+  (* FIXME Webs_fs do something nice in send_file maybe *)
+  match Fpath.take_ext ~multi:false file with
   | ".css" | ".js" | ".woff2" ->
       (* FIXME versioning scheme, note something was done in Static_file *)
       let forever = "public, max-age=31536000, immutable" in
       let hs = Http.Headers.(def cache_control) forever Http.Headers.empty in
-      Http.Response.override_headers ~by:hs resp
+      Ok (Http.Response.override_headers ~by:hs response)
   | ".html" ->
       let ctrl = "max-age=0" in
       let hs = Http.Headers.(def cache_control) ctrl Http.Headers.empty in
-      Http.Response.override_headers ~by:hs resp
-  | _ -> resp
-  in
-  Ok resp
+      Ok (Http.Response.override_headers ~by:hs response)
+  | _ -> Ok response
